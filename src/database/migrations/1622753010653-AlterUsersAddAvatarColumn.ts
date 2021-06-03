a647608fd3abc2108ef0b52188081bea6525625a
marcelogaldino <marcelinhomgo@gmail.com>
2021-06-03 19:09:10 -0300

    feat: user avatar upload

---
+++ src/database/migrations/1622753010653-AlterUsersAddAvatarColumn.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUsersAddAvatarColumn1622753010653
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "avatar",
+        type: "varchar",
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "avatar");
+  }
+}
